1f6a5f949acbf3086196dbc10257996c363e47cc
Juan Oxmar <joxmar.ramirez@gmail.com>
2020-10-29 16:53:18 -0700

    Bubble Sort

---
+++ Section 11: Bubble Sort/bubbleSort.js
@@ -0,0 +1,24 @@
+function bubbleSort(list) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let j = list.length; j > 0; j--) {
+    let noSwap = true;
+    for (let i = 0; i < j - 1; i++) {
+      if (list[i] > list[i + 1]) {
+        swap(list, i, i + 1);
+        noSwap = false;
+      }
+    }
+    if (noSwap) {
+      break;
+    }
+  }
+
+  return list;
+}
+
+console.log(
+  bubbleSort([1000, 7, 2, 4, 9, 5, 8, 22, 67, 99, 23, 45, 67, 85, 1])
+);
