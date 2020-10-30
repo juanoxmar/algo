948ba8789f5911b27f37cd0b81a43296cee39709
Juan Oxmar <joxmar.ramirez@gmail.com>
2020-10-29 21:24:55 -0700

    Refactored Insertion Sort

--- Section 13: Insertion Sort/insertionSort.js
+++ Section 13: Insertion Sort/insertionSort.js
@@ -1,14 +1,11 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
-    for (let j = i - 1; j >= 0; j--) {
-      if (arr[i] > arr[j]) {
-        arr.splice(j + 1, 0, ...arr.splice(i, 1));
-        break;
-      }
-      if (j === 0) {
-        arr.unshift(...arr.splice(i, 1));
-      }
+    const currentValue = arr[i];
+    let j;
+    for (j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
+      arr[j + 1] = arr[j];
     }
+    arr[j + 1] = currentValue;
   }
   return arr;
 }
