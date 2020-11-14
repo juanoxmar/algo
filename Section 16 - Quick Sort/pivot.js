7e1a12a1e50cd850efc753d67a34ef8ef7cadff8
Juan Oxmar <joxmar.ramirez@gmail.com>
2020-11-13 19:08:40 -0800

    Updates

--- Section 16 - Quick Sort/pivot.js
+++ Section 16 - Quick Sort/pivot.js
@@ -3,21 +3,23 @@
  * @returns {number[]}
  */
 
-function pivot(arr, start, end) {
+function pivot(arr, start = 0, end = arr.length - 1) {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
   };
-  let swapCount = 0;
-  for (let i = start + 1; i < end; i++) {
+
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
     if (arr[start] > arr[i]) {
-      swapCount += 1;
-      swap(arr, swapCount, i);
+      swapIdx += 1;
+      swap(arr, swapIdx, i);
     }
   }
-  swap(arr, start, swapCount);
+  swap(arr, start, swapIdx);
   console.log(arr);
 
-  return swapCount;
+  return swapIdx;
 }
 const arr = [7, 2, 4, 9, 5, 8, 1000, 22, 99, 23, 45, 67, 85, 1];
-console.log(pivot(arr, 0, arr.length));
+console.log(pivot(arr));
--- Section 16 - Quick Sort/quickSort.js
+++ Section 16 - Quick Sort/quickSort.js
@@ -14,18 +14,21 @@ function quickSort(arr) {
  * @returns {number}
  */
 
-function pivot(arr, start, end) {
+function pivot(arr, start = 0, end = arr.length - 1) {
   const swap = (arr, idx1, idx2) => {
-    [arr[idx1], arr[idx2]] = [arr[idx2][idx1]];
-  
-  let swapCount = 0;
-  for (let i = start + 1; i < end; i++) {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
     if (arr[start] > arr[i]) {
-      swapCount += 1;
-      swap(arr, swapCount, i);
+      swapIdx += 1;
+      swap(arr, swapIdx, i);
     }
   }
-  swap(arr, start, swapCount);
+  swap(arr, start, swapIdx);
+  console.log(arr);
 
-  return swapCount;
+  return swapIdx;
 }
